26e205fc0737ea26cbddef58606e73f9fc51b112
Adel Nour <ans9868@nyu.edu>
2025-04-27 18:15:13 -0400

    tested, all works

---
+++ frontend/src/services/artService.js
@@ -0,0 +1,37 @@
+import authService from './authService';
+
+const API_URL = 'http://localhost:4000/api';
+
+class ArtService {
+  async getArtDetails(artworkId) {
+    const token = authService.getToken();
+    console.log('Token:', token);
+    if (!token) {
+      throw new Error('No authentication token found (getting art details)');
+    }
+
+    try {
+      const response = await fetch(`${API_URL}/details`, {
+        method: 'POST',
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ artworkId }),
+      }); // could be string or int .... don't know what will work or not 
+
+      if (!response.ok) {
+        throw new Error('Failed to fetch profile data');
+      }
+
+      const data = await response.json();
+      console.log('Profile Data Response:', data);
+      return data;
+    } catch (error) {
+      console.error('Profile fetch error:', error);
+      throw error;
+    }
+  }
+}
+
+export default new ArtService(); 
