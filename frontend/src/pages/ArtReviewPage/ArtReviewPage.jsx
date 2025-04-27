0b14774df87856656832b0404742fee6694af2de
Adel Nour <ans9868@nyu.edu>
2025-04-27 13:44:48 -0400

    getting profile in artreview

--- frontend/src/pages/ArtReviewPage/ArtReviewPage.jsx
+++ frontend/src/pages/ArtReviewPage/ArtReviewPage.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
 import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import profileService from '../../services/profileService';
 import './ArtReviewPage.css';
+import { useAuth } from '../../context/AuthContext';
 
 const ArtReviewPage = () => {
-    console.log('ArtReviewPage');
+    const { user } = useAuth();
     const { artId } = useParams();
+    const [profileData, setProfileData] = useState(null);
+
+    useEffect(() => {
+        const fetchProfileData = async () => {
+            if (user?.userid) {
+              try {
+                const data = await profileService.getProfile(user.userid);
+                setProfileData(data);
+              } catch (error) {
+                console.error('Error fetching profile:', error);
+              }
+            }
+          };
+      
+          fetchProfileData();
+    }, [artId]);
 
     return (
         <div className="art-review-page">
